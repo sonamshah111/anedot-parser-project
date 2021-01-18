6341e2cdc0e2d2c6c3251cff74499f5236b023a0
Sonam Shah <sonam.shah1687@gmail.com>
2021-01-18 16:13:45 -0600

    Step3: Printed parsed HTML raw results.

--- cl_reader.rb
+++ cl_reader.rb
@@ -1,2 +1,7 @@
 require 'nokogiri'
 require 'open-uri' # Used to parse directly from URI instead of HTML string
+
+craiglist_raw_html = Nokogiri::HTML.parse(open('https://seattle.craigslist.org/search/cta?query=jeep')) # Nokogiri::HTML.parse used for parsing directly from URL 
+
+# Step3 : Parse https://seattle.craigslist.org/search/cta?query=jeep and print raw results
+print craiglist_raw_html 
