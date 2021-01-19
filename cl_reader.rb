6355a743f9a76b29b24fcfc22c24f3cf2d78999c
Sonam Shah <sonam.shah1687@gmail.com>
2021-01-18 18:27:16 -0600

    Step6: Added total for amounts and displayed result..

--- cl_reader.rb
+++ cl_reader.rb
@@ -7,13 +7,20 @@ craiglist_raw_html = Nokogiri::HTML.parse(open('https://seattle.craigslist.org/s
 #print craiglist_raw_html 
 
 # Step5: Print each item with Model Year - Date - Title 
-puts "Model Year -  Date       -       Title"
+puts "Model Year -  Date       -       Title                                          --   Price"
 100.times { print "-" }
-print "\n"
+puts
+total = 0
 craiglist_raw_html.css('div.result-info').each do |car_detail|
   date_info = Date.parse car_detail.css('time')[0]['datetime'] 
   model = car_detail.css('h3.result-heading').text.scan(/\b\d{4}\b/).first ? car_detail.css('h3.result-heading').text.scan(/\b\d{4}\b/).first : ""
   title = car_detail.css('h3.result-heading').text.strip! #removing extra space in title
-  puts model + "       -  " + date_info.to_s + " -    " + title.gsub(model, "")
+  price = car_detail.css('span.result-price').text
+  puts model + "       -  " + date_info.to_s + " -    " + title.gsub(model, "") + "                -   "+ price
+  price = price.gsub(/\D/,'').to_i
+  total += price
  end	
-
+100.times { print "-" }
+# Step6: 
+puts 
+puts "Total:  $#{"%.2f" % total}"
